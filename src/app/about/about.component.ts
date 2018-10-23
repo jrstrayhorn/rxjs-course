18fcda3ff687baade656e882ac8562adeb31e73e
J.R. Strayhorn <jrstrayhorn@gmail.com>
2018-10-23 15:23:14 -0400

    starting subject lesson

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -13,7 +13,23 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
+        const subject = new Subject();
 
+        // this emit value of subject
+        const series$ = subject.asObservable();
+
+        series$.subscribe(console.log);
+
+        // really we should try to create observables as close to the source as possible
+        // for a promise use, fromPromise
+        // from browser event, from(document, 'keyup')
+
+        subject.next(1);
+        subject.next(2);
+        subject.next(3);
+        subject.complete();
+
+        
     }
 
 
